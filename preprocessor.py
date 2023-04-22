e9a0a235923f1b307caf326d688002bf97a23dba
Samvel <samo.opkar1@gmail.com>
2023-04-23 01:36:30 +0400

    fixing communication with pipeline

--- preprocessor.py
+++ preprocessor.py
@@ -1,52 +1,52 @@
 # import numpy as np
 # import pandas as pd
-#
+
 # class Preprocessor:
 #     def __init__(self):
 #         self.column_means = None
 #         self.min_values = None
 #         self.max_values = None
-#
+
 #         # Initialize target name
 #         self.target_column = "In-hospital_death"
-#
+
 #     def fit(self, X):
 #         # Calculate column means for filling NaN values
 #         self.column_means = X.mean()
-#
+
 #         # Calculate min and max values for scaling
 #         self.min_values = np.nanmin(X, axis=0)
 #         self.max_values = np.nanmax(X, axis=0)
-#
+
 #     def transform(self, X):
 #         # Fill NaN values with column means
 #         # X_filled = np.where(np.isnan(X_none_to_nan), self.column_means, X_none_to_nan)
 #         # X_filled = np.where(np.isnan(X), self.column_means, X)
-#
+
 #         # # Scale features using min-max scaling
 #         # X_scaled = (X_filled - self.min_values) / (self.max_values - self.min_values)
 #         # X_filled = np.where(np.isnan(X_scaled), self.column_means, X_scaled)
-#
-#
+
+
 #         # Pandas initialization
 #         X.fillna(self.column_means, inplace=True)
-#
+
 #         # TODO: Must be written scaling part:
-#
+
 #         ...
-#
+
 #         #################3
-#
+
 #         # Train mode checking and return
 #         if self.target_column in X.columns:
 #             X_transformed = X.drop(self.target_column, axis=1)
 #             y = X[self.target_column]
 #             return X_transformed, y
-#
+
 #         print(X.columns)
 #         return X
 #         ###################
-#
+
 #     def fit_transform(self, X):
 #         self.fit(X)
 #         return self.transform(X)
@@ -55,32 +55,18 @@ import numpy as np
 import pandas as pd
 
 
-class Preprocessor5:
+class Preprocessor:
     def __init__(self):
         self.column_means = None
-        self.target_column = "In-hospital_death"
         self.min_values = None
         self.max_values = None
 
     def fit(self, df):
-
-        if self.target_column in df.columns:
-            # Calculate separate means for each group based on the first column
-            first_group = df[df.iloc[:, 0] == 0]
-            second_group = df[df.iloc[:, 0] == 1]
-
-            first_mean = first_group.mean()
-            second_mean = second_group.mean()
-
-            self.column_means = (first_mean + second_mean) / 2
-        else:
-            self.column_means = df.mean()
-
-        # Calculate min and max values for scaling
+        self.column_means = df.mean()
         self.min_values = df.min()
         self.max_values = df.max()
 
-    def transform(self, df, poly=True):
+    def transform(self, df, poly=False):
         # Fill NaN values with column means
         df_filled = df.fillna(self.column_means)  # , inplace=True
 
@@ -102,11 +88,11 @@ class Preprocessor5:
         else:
             df_extended = df_scaled
 
+        # Go to pipeline code, will be deleted
         # Train mode checking
-        if self.target_column in df.columns:
-            df_extended = df.drop(self.target_column, axis=1)
-            y = df[self.target_column]
-            return df_extended, y
+        # if self.target_column in df.columns:
+        #     df_extended = df.drop(self.target_column, axis=1)
+        #     y = df[self.target_column]
+        #     return df_extended, y
 
         return df_extended
-
